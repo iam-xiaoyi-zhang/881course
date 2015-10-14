922606f8c4497267996dc1709c794dbf9b2399f5
Xiaoyi Zhang <iamxyz@bu.edu>
2015-10-13 20:58:31 -0400

    Author: Xiaoyi Zhang 2015/10/13
    modified file: exp-poi-gam-xyz
    .R
    
    I modified the exp-poi-gam-xyz
    .R
    added ggplot into it and changed some other details

--- exp-poi-gam-xyz.R
+++ exp-poi-gam-xyz.R
@@ -1,11 +1,8 @@
 library(ggplot2)
 library(qualityTools)
-
+#set.seed(50)
 
 rsample <- function(typ, lam=1, rep=1, exp.max=0, poi.max=1, gam.alpha=1){ 
-  # the lam parameter is shared by exp,poi,gam distribution.
-  # "exp." means this parameter is used only when generating exponetial random samples
-  # so does the "poi." or "gam."
   # parameter:
   #   typ: the type of distribution, c("exp", "poi", "gam")
   #   lam: the common parameter in exp. poi. and gamma distribution.
@@ -15,34 +12,44 @@ rsample <- function(typ, lam=1, rep=1, exp.max=0, poi.max=1, gam.alpha=1){
   #   gam.alpha: the first parameter of gamma distribution, number of exponential variables.
   # return:
   #   a sample vector
+  # Note: the lam parameter is shared by exp,poi,gam distribution.
+  #       "exp." means this parameter is used only when generating exponetial random samples
+  #       so does the "poi." or "gam."
   if(rep<=0){warning("rep must be integer greater than 0")}
   if(typ=="exp"){ # exp: rep, lam, exp.max
     if(exp.max<=0){ 
-      rt <- rexp(rep,lam)
+      ret <- rexp(rep,lam)
     } else {
-      rt <- rexp.max(exp.max, lam)
+      ret <- rexp.max(exp.max, lam)
     }
   } else if(typ=="poi"){ # poi: rep, lam, poi.max
     if(poi.max<=0){warning("poi.max should be greater than zero")}
     # poisson variable is length of exp. with sum less than certain threshold.
-    rt <- NULL
+    ret <- NULL
     for(i in 1:rep){
       tmp <- rexp.max(poi.max, lam)
       len <- length(tmp)
-      rt <- c(rt, len)
+      ret <- c(ret, len)
     }
   } else if(typ=="gam"){ # gam: rep, lam, gam.alpha
     if(gam.alpha<=0){warning("gam.alpha should be greater than zero")}
     # gamma variable is sum of exp. with certain length
-    rt <- NULL
+    ret <- NULL
     for(i in 1:rep){
-      rt <- c(rt, sum(rexp(gam.alpha, lam)))
+      ret <- c(ret, sum(rexp(gam.alpha, lam)))
     }
   } else {warning("wrong type")}
-  return(rt)
+  
+  meanret <- mean(ret)
+  print(meanret)
+  df <- data.frame(ret=ret, meanret=meanret)
+  p <- ggplot(data=df)
+  p <- p + geom_density(aes(x=ret), binwidth=1) + xlab(typ) + geom_vline(aes(xintercept=meanret))
+  print(p)
+  return(ret)
 }
 
-# I don't use this function directly. 
+# I don't use this function directly. ggplot() + geom_density(aes(x=dat), binwidth=1)
 # it is only invoked in rsample()
 rexp.max <- function(max, lam){
   t=0
@@ -52,18 +59,15 @@ rexp.max <- function(max, lam){
     a <- c(a, inter)
     t <- t + inter
   }
-  rt <- a[1:(length(a)-1)]
-  return(rt)
+  ret <- a[1:(length(a)-1)]
+  return(ret)
 }
 
-rsample("nba")
-rsample("exp", rep=5, lam=-2)
-rsample("exp", rep=5, lam=2, exp.max=-4)
-rsample("exp", rep=5, lam=2, exp.max=10)
-rsample("exp", rep=-4, lam=3)
-rsample("exp", rep=-4, lam=3, exp.max=3)
-rsample("poi", rep=5, lam=2, poi.max=3)
-rsample("poi", rep=5, lam=2, poi.max=-3)
-rsample("gam", rep=5, lam=2, gam.alpha=-4)
-rsample("gam", rep=5, lam=2, gam.alpha=1.3)
-rsample("gam", rep=5, lam=2, gam.alpha="HAHAHA")
+
+dat <- rsample("exp", rep=1000, lam=2)
+dat <- rsample("exp", rep=1000, lam=2, exp.max=10)
+dat <- rsample("poi", rep=1000, lam=2, poi.max=3)
+dat <- rsample("gam", rep=1000, lam=2, gam.alpha=4)
+
+
+
